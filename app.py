1fb92af2150a04336081fe7ca7d22c16918a2267
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-01 22:30:26 +0100

    initial

---
+++ app.py
@@ -0,0 +1,39 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+ratings = spark.read.csv("dataset/rating_test.csv", header=True,inferSchema=True,sep=",")
+anime = spark.read.csv("dataset/anime.csv", header=True,inferSchema=True,sep=",",escape="\"")
+
+ratings = ratings.join(anime,ratings.anime_id==anime.ID,"inner").select(ratings["*"],anime["Type"])
+# ratings.show()
+ratings_tv= ratings.filter(ratings['Type']=="TV")
+ratings_tv.show()
+ratings_movies= ratings.filter(ratings['Type']=="Movie")
+ratings_movies.show()
+ratings = [ratings_movies]
+for r in ratings:
+# Dividimos el dataset en train/test
+    (training,test) = r.randomSplit([0.8, 0.2])
+# Entrenamos el modelo. La estrategia coldstartcon 'drop' descarta valores NaN en evaluación
+    als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="anime_id", ratingCol="rating", coldStartStrategy="drop")
+    model=als.fit(training)
+# Evaluamos el modelo con RMSE
+    predictions = model.transform(test)
+    evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
+    rmse = evaluator.evaluate(predictions)
+    users = r.filter(r["user_id"]==0)
+    userSubsetRecs = model.recommendForUserSubset(users, 5)
+    movies=userSubsetRecs.first()['recommendations']
+    print(movies)
+# userSubsetRecs.show(truncate=False)
+    recommendations=[]
+    for movie in movies:
+        recommendations.append(movie['anime_id'])
+        # print(movie['anime_id'])
+    anime.filter((anime.ID).isin(recommendations)).select('ID','English name','Japanese name').show()
+
