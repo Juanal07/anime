95604bdbc9500222f15632ea7b54bb6d5a68efdb
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-05 00:46:22 +0100

    a

--- app.py
+++ app.py
@@ -5,29 +5,25 @@ import sys
 
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
-user_id_target=0
-# user_id_target=666666
+# user_id_target=0
+user_id_target=666666
 
-ratings = spark.read.csv("dataset/rating_test.csv", header=True,inferSchema=True,sep=",")
-anime = spark.read.csv("dataset/anime.csv", header=True,inferSchema=True,sep=",",escape="\"")
-# ratings = spark.read.csv("gs://anime-jar/rating_complete.csv", header=True,inferSchema=True,sep=",")
-# anime = spark.read.csv("gs://anime-jarr/anime.csv", header=True,inferSchema=True,sep=",",escape="\"")
+# ratings = spark.read.csv("dataset/rating_test.csv", header=True,inferSchema=True,sep=",")
+# anime = spark.read.csv("dataset/anime.csv", header=True,inferSchema=True,sep=",",escape="\"")
+ratings = spark.read.csv("gs://anime-jar/rating_complete.csv", header=True,inferSchema=True,sep=",")
+anime = spark.read.csv("gs://anime-jarr/anime.csv", header=True,inferSchema=True,sep=",",escape="\"")
 
 ratings = ratings.join(anime,ratings.anime_id==anime.ID,"inner").select(ratings["*"],anime["Type"])
 ratings_tv= ratings.filter(ratings['Type']=="TV")
 ratings_movies= ratings.filter(ratings['Type']=="Movie")
 ratings = [ratings_movies,ratings_tv]
-# ratings = [ratings_tv]
 names = ["peliculas.txt","series.txt"]
 
 i=0
 for r in ratings:
-    # Dividimos el dataset en train/test
     (training,test) = r.randomSplit([0.8, 0.2])
-    # Entrenamos el modelo. La estrategia coldstartcon 'drop' descarta valores NaN en evaluación
     als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="anime_id", ratingCol="rating", coldStartStrategy="drop")
     model=als.fit(training)
-    # Evaluamos el modelo con RMSE
     predictions = model.transform(test)
     evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
     rmse = evaluator.evaluate(predictions)
@@ -38,8 +34,8 @@ for r in ratings:
     for movie in movies:
         recommendations.append(movie['anime_id'])
     result = anime.filter((anime.ID).isin(recommendations)).select('ID','English name','Japanese name')
-    # result.write.format("com.databricks.spark.csv").option("header", "true").save("output_{}".format(i))
-    sys.stdout = open(names[i], "w")
+    # sys.stdout = open("output/"+names[i], "w+")
+    sys.stdout = open("gs://anime-jarr/{}".format(names[i]), "w+")
     result.show(truncate=False)
     sys.stdout.close()
     i+=1
