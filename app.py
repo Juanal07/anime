b7e777424dfd5d055958eddf9fa548cc3ecc5310
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-11 11:17:11 +0100

    first traing then split

--- app.py
+++ app.py
@@ -12,32 +12,52 @@ user_id_target=666666
 ratings = spark.read.csv("gs://anime-jarr/rating_complete.csv", header=True,inferSchema=True,sep=",")
 anime = spark.read.csv("gs://anime-jarr/anime.csv", header=True,inferSchema=True,sep=",",escape="\"")
 
+# ratings = ratings.join(anime,ratings.anime_id==anime.ID,"inner").select(ratings["*"],anime["Type"])
+# ratings_tv= ratings.filter(ratings['Type']=="TV")
+# ratings_movies= ratings.filter(ratings['Type']=="Movie")
+# ratings = [ratings_movies,ratings_tv]
+# names = ["peliculas.txt","series.txt"]
+#
+# i=0
+# for r in ratings:
+#     (training,test) = r.randomSplit([0.8, 0.2])
+#     als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="anime_id", ratingCol="rating", coldStartStrategy="drop")
+#     model=als.fit(training)
+#     users = r.filter(r["user_id"]==user_id_target)
+#     userSubsetRecs = model.recommendForUserSubset(users, 5)
+#     movies=userSubsetRecs.first()['recommendations']
+#     recommendations=[]
+#     for movie in movies:
+#         recommendations.append(movie['anime_id'])
+#     result = anime.filter((anime.ID).isin(recommendations)).select('ID','English name','Japanese name')
+#     sys.stdout = open("output/"+names[i], "w+")
+#     result.show(truncate=False)
+#     sys.stdout.close()
+#     blob = bucket.blob("output/"+names[i])
+#     blob.upload_from_filename("output/"+names[i])
+#     i+=1
+
 ratings = ratings.join(anime,ratings.anime_id==anime.ID,"inner").select(ratings["*"],anime["Type"])
-ratings_tv= ratings.filter(ratings['Type']=="TV")
-ratings_movies= ratings.filter(ratings['Type']=="Movie")
-ratings = [ratings_movies,ratings_tv]
+
+# ratings_tv= ratings.filter(ratings['Type']=="TV")
+# ratings_movies= ratings.filter(ratings['Type']=="Movie")
+# ratings = [ratings_movies,ratings_tv]
 names = ["peliculas.txt","series.txt"]
 
-i=0
-for r in ratings:
-    (training,test) = r.randomSplit([0.8, 0.2])
-    als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="anime_id", ratingCol="rating", coldStartStrategy="drop")
-    model=als.fit(training)
-    # predictions = model.transform(test)
-    # evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
-    # rmse = evaluator.evaluate(predictions)
-    users = r.filter(r["user_id"]==user_id_target)
-    userSubsetRecs = model.recommendForUserSubset(users, 5)
-    # userSubsetRecs.show(truncate=False)
-    movies=userSubsetRecs.first()['recommendations']
-    recommendations=[]
-    for movie in movies:
-        recommendations.append(movie['anime_id'])
-    result = anime.filter((anime.ID).isin(recommendations)).select('ID','English name','Japanese name')
-    sys.stdout = open("output/"+names[i], "w+")
-    result.show(truncate=False)
-    sys.stdout.close()
-    blob = bucket.blob("output/"+names[i])
-    blob.upload_from_filename("output/"+names[i])
-    i+=1
+(training,test) = ratings.randomSplit([0.8, 0.2])
+als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="anime_id", ratingCol="rating", coldStartStrategy="drop")
+model=als.fit(training)
+users = ratings.filter(ratings["user_id"]==user_id_target)
+userSubsetRecs = model.recommendForUserSubset(users, 5)
+movies=userSubsetRecs.first()['recommendations']
+recommendations=[]
+for movie in movies:
+    recommendations.append(movie['anime_id'])
+result = anime.filter((anime.ID).isin(recommendations)).select('ID','English name','Japanese name')
+
+sys.stdout = open("output/"+names[1], "w+")
+result.show(truncate=False)
+sys.stdout.close()
+blob = bucket.blob("output/"+names[1])
+blob.upload_from_filename("output/"+names[1])
 
