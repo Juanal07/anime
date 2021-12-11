eeeb9a11843a90b581406a5c65a51a89f022e47d
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-11 19:26:12 +0100

    auto push

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import requests
 import time
 
 spark = SparkSession.builder.master("local[*]").getOrCreate()
+
 storage_client = storage.Client()
 bucket = storage_client.bucket('anime-jarr')
+
 user_id_target=666666
 
 ratings = spark.read.csv("gs://anime-jarr/rating_complete.csv", header=True,inferSchema=True,sep=",")
@@ -28,29 +30,31 @@ result = anime.filter((anime.ID).isin(recommendations)).select('ID','Name','Japa
 df_tv = result.filter(result['Type']=="TV").toPandas()
 df_movie = result.filter(result['Type']=="Movie").toPandas()
 
-def save(df,name):
-    local_df= df[0:5]
-    images = []
-    videos = []
-    for i in range(5):
-        print(str(local_df.iloc[i].loc['ID']))
-        r = requests.get('https://api.jikan.moe/v3/anime/'+str(local_df.iloc[i].loc['ID']))
-        image=str(r.json()['image_url'])
-        print(r.json()['image_url'])
-        video=str(r.json()['trailer_url'])
-        print(r.json()['trailer_url'])
-        images.append('<img src="'+image+'" />')
-        videos.append('<iframe width="420" height="315" src="'+video+'"></iframe>')
-        time.sleep(2)
-    local_df['Image'] = images
-    local_df['Trailer'] = videos
-    local_df.to_csv("output/{}.csv".format(name))
-    blob = bucket.blob("output/{}.csv".format(name))
-    blob.upload_from_filename("output/{}.csv".format(name))
-    local_df.to_html("output/{}.html".format(name),escape=False)
-    blob = bucket.blob("output/{}.html".format(name))
-    blob.upload_from_filename("output/{}.html".format(name))
+# def save(df,name):
+name = 'Series'
 
-save(df_tv, 'Series')
+local_df= df_tv[0:5]
+images = []
+videos = []
+for i in range(5):
+    # print(str(local_df.iloc[i].loc['ID']))
+    r = requests.get('https://api.jikan.moe/v3/anime/'+str(local_df.iloc[i].loc['ID']))
+    image=str(r.json()['image_url'])
+    print(r.json()['image_url'])
+    video=str(r.json()['trailer_url'])
+    print(r.json()['trailer_url'])
+    images.append('<img src="'+image+'" />')
+    videos.append('<iframe width="420" height="315" src="'+video+'"></iframe>')
+    time.sleep(2)
+local_df['Image'] = images
+local_df['Trailer'] = videos
+local_df.to_csv("output/{}.csv".format(name))
+blob = bucket.blob("output/{}.csv".format(name))
+blob.upload_from_filename("output/{}.csv".format(name))
+local_df.to_html("output/{}.html".format(name),escape=False)
+blob = bucket.blob("output/{}.html".format(name))
+blob.upload_from_filename("output/{}.html".format(name))
+
+# save(df_tv, 'Series')
 # save(df_movie, 'Películas')
 
